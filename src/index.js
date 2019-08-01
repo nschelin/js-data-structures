ef0a3d5713cebb0e5347d6642c54e6f81774b7a7
nschelin <n.schelin@outlook.com>
2019-07-31 22:24:03 -0700

    add queue

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
-import "./styles.css";
+import { Queue } from './ds/queue';
+import { Stack } from './ds/stack';
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use Parcel to bundle this sandbox, you can find more info about Parcel
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
+const queue = new Queue();
+const stack = new Stack();
+
+queue.enqueue('queue 1');
+queue.enqueue('queue 2');
+queue.enqueue('queue 3');
+queue.enqueue('queue 4');
+queue.enqueue('queue 5');
+
+const app = document.getElementById('app');
+
+app.innerHTML = `
+  queue size: ${queue.size()}
 `;
