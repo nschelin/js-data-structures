bcd77a45a5ae113c9eb958c06405170b29a6bc40
nschelin <n.schelin@outlook.com>
2019-07-31 22:25:46 -0700

    add stack

--- src/index.js
+++ src/index.js
@@ -10,8 +10,15 @@ queue.enqueue('queue 3');
 queue.enqueue('queue 4');
 queue.enqueue('queue 5');
 
+stack.push('stack 1');
+stack.push('stack 2');
+stack.push('stack 3');
+stack.push('stack 4');
+stack.push('stack 5');
+
 const app = document.getElementById('app');
 
 app.innerHTML = `
-  queue size: ${queue.size()}
+  <div>queue size: ${queue.size()}</div> 
+  <div>stack size: ${stack.size()}</div>
 `;
